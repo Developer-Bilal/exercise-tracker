1247c7c5d09416dbb96d64c1965b37f79bd1332f
Muhammad Bilal <bilalchanna67@gmail.com>
2024-10-13 17:33:30 +0500

    minor updates

--- server/app.js
+++ server/app.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 5555;
 app.use(express.json());
 app.use("/exercise", router);
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Home type /exercise in path to get data!" });
+});
+
+app.get("*", (req, res) => {
+  res.status.json({ message: "This route does not exist!" });
+});
+
 const start = async () => {
   const connectDB = await mongoose.connect(process.env.MONGO_URI);
 
